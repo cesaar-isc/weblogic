ebd5ca4d108a683b246877f6423aaf9847ca3c8f
Cesar <cesaar.isc@gmail.com>
2020-09-16 19:26:29 -0500

    creacion de proyecto

---
+++ src/main/java/com/cesaarisc/weblogic/config/MvcConfiguration.java
@@ -0,0 +1,28 @@
+package com.cesaarisc.weblogic.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan(basePackages="com.cesaarisc.weblogic")
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer{
+
+
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	
+}
